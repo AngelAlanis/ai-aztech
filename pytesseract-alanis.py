99065c3aac2c223fb85d09159da95fab99930b59
ahangel256@gmail.com <ahangel256@gmail.com>
2023-05-04 11:51:10 -0600

    Añadido método para guardar imagen procesada.

--- pytesseract-alanis.py
+++ pytesseract-alanis.py
@@ -1,5 +1,6 @@
 import pytesseract as tess
 from PIL import Image
+import PIL
 import numpy as np
 import cv2
 
@@ -9,9 +10,11 @@ def preprocesar_imagen(imagen):
 
     imagen = np.array(imagen)
     imagen = cv2.resize(imagen, None, fx=2, fy=2)
+
     imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
-    imagen = cv2.threshold(imagen, 127, 255, cv2.THRESH_BINARY)[1]
-    imagen = cv2.erode(imagen, kernel, iterations=1)  
+    imagen = cv2.blur(imagen,(3,3))
+    imagen = cv2.Canny(imagen, 150, 200)
+    imagen = cv2.dilate(imagen, None, iterations=1)
 
     return imagen
 
@@ -21,6 +24,9 @@ def imprimir_resultado(resultado):
         print("No se detectó nada.")
     else:
          print(resultado)
+
+def guardar_imagen(nombre_archivo, imagen):
+    cv2.imwrite(nombre_archivo, imagen)
 
 # Definir el PATH De PyTesseract.
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -35,11 +41,15 @@ imagen1 = preprocesar_imagen(imagen1)
 imagen2 = preprocesar_imagen(imagen2)
 imagen3 = preprocesar_imagen(imagen3)
 
+#Guardando la imagen
+guardar_imagen('test1.png', imagen1)
+guardar_imagen('test2.png', imagen2)
+guardar_imagen('test3.png', imagen3)
 
 #Obtener los resultados
-result1 = tess.image_to_string(imagen1, lang='spa+eng')
-result2 = tess.image_to_string(imagen2, lang='spa+eng')
-result3 = tess.image_to_string(imagen3, lang='spa+eng')
+result1 = tess.image_to_string(imagen1, lang='spa+eng', config='--psm 11')
+result2 = tess.image_to_string(imagen2, lang='spa+eng', config='--psm 12')
+result3 = tess.image_to_string(imagen3, lang='spa+eng', config='--psm 11')
 
 #Imprimir los resultados.
 imprimir_resultado(result1)
