ad5fac938d8d2129cd7df4d66083b26de1177a52
Eder Ceniceros <130949300+EderCeniceros@users.noreply.github.com>
2023-05-06 17:18:29 -0600

    Update yolo.py
    Se añadieron listas de resultados y detección de monedas

--- yolo.py
+++ yolo.py
@@ -7,12 +7,12 @@ import os
 current_dir = os.path.abspath(os.path.dirname(__file__))
 
 # Cargar las etiquetas de los objetos
-labels_path = os.path.join(current_dir,'coco.names')
+labels_path = os.path.join(current_dir,'aztech.names')
 with open(labels_path, "r") as f:
     labels = [line.strip() for line in f.readlines()]
 
 # Cargar modelo
-model = torch.hub.load('ultralytics/yolov5', 'custom', path=os.path.join(current_dir,'prueba.pt'), force_reload=True)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path=os.path.join(current_dir,'aztech.pt'), force_reload=True)
 
 # Cargar imagen
 image = cv2.imread(os.path.join(current_dir,'imagen.jpg'))
@@ -21,6 +21,10 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 # Realizar detección de objetos
 results = model(image)
 detections = results.xyxy[0].numpy()
+
+# Listas para almacenar los objetos detectados y los porcentajes de confianza
+objetos_detectados = []
+porcentajes_confianza = []
 
 # Mostrar las detecciones en la imagen
 for detection in detections:
@@ -36,6 +40,14 @@ for detection in detections:
     # Mostrar el nombre de la detección
     cv2.putText(image, f"{label}: {confidence:.2f}", (x1, y1 - 10),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    # Almacenar el objeto detectado y su porcentaje de confianza
+    objetos_detectados.append(label)
+    porcentajes_confianza.append(round(confidence, 2))
+
+# Imprimir las listas de objetos detectados y porcentajes de confianza
+print("Objetos detectados:", objetos_detectados)
+print("Porcentajes de confianza:", porcentajes_confianza)
 
 # Mostrar la imagen con las detecciones
 cv2.imshow("Detecciones", image)
