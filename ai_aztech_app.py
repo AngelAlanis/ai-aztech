a03f59bd4d19d3dc1e4718e47180aefa60a9673d
ahangel256@gmail.com <ahangel256@gmail.com>
2023-05-18 17:31:11 -0600

    Timestamps de la ejecución agregados.

--- ai_aztech_app.py
+++ ai_aztech_app.py
@@ -1,9 +1,8 @@
 # Importación de librerias
 import os
 import re
-import time
+from datetime import datetime
 
-import cv2
 import nltk
 import numpy as np
 import pytesseract
@@ -22,27 +21,37 @@ pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesse
 IDIOMA_USUARIO = "es"
 
 # Hace la conexion con la api por medio de internet.
+print(datetime.now().strftime("%H:%M:%S.%f")[
+      :-3], "Haciendo conexión con la API de LibreTranslate.")
 traductor = LibreTranslateAPI('https://libretranslate.org/')
+print(datetime.now().strftime("%H:%M:%S.%f")[
+      :-3], "Conexión con la API terminada.")
 
 # Descargar componentes necesarios en caso de que no estén presentes
+print(datetime.now().strftime("%H:%M:%S.%f")
+      [:-3], "Comprobando paquetes de nltk.")
 nltk.download('punkt')
 nltk.download('stopwords')
+print(datetime.now().strftime("%H:%M:%S.%f")[
+      :-3], "Comprobación de paquetes nltk terminados.")
 
 # Banderas para el flujo del programa
 is_detectando_texto = True
 
 # Imagen
+print(datetime.now().strftime("%H:%M:%S.%f")[:-3], "Cargando imagen.")
 imagen = Image.open('resources/texto.png')
+print(datetime.now().strftime("%H:%M:%S.%f")
+      [:-3], "Imagen cargada correctamente.")
+
 
 def limpiar_texto(text):
+    print(datetime.now().strftime("%H:%M:%S.%f")[:-3], "Limpiando texto.")
     # Eliminar caracteres no deseados
     text = re.sub(r'[^\w\s]', '', text)
 
     # Convertir el texto a minúsculas
     text = text.lower()
-
-    # Eliminar acentos y diacríticos
-    text = unidecode(text)
 
     # Segmentación de palabras y eliminación de palabras vacías
     stop_words = set(stopwords.words('spanish'))
@@ -50,16 +59,24 @@ def limpiar_texto(text):
     filtered_words = [word for word in words if word not in stop_words]
     text = ' '.join(filtered_words)
 
+    print(datetime.now().strftime("%H:%M:%S.%f")[:-3], "Texto limpiado.")
     return text
 
 
 def detectar_idioma_y_traducir(texto):
     if texto != '':
         print(texto)
+        print(datetime.now().strftime("%H:%M:%S.%f")
+              [:-3], "Detectando idioma.")
         idioma_detectado = detect(texto)
+        print(datetime.now().strftime("%H:%M:%S.%f")[:-3], "Idioma detectado.")
 
         if idioma_detectado != IDIOMA_USUARIO:
+            print(datetime.now().strftime("%H:%M:%S.%f")
+                  [:-3], "Traduciendo texto.")
             texto = traducir_texto(texto, idioma_detectado)
+            print(datetime.now().strftime("%H:%M:%S.%f")
+                  [:-3], "Texto traducido.")
             print("Texto traducido:", texto)
 
     return texto
@@ -79,15 +96,22 @@ def procesar_imagen():
     current_dir = os.path.abspath(os.path.dirname(__file__))
 
     # Cargar modelo
+    print(datetime.now().strftime("%H:%M:%S.%f")
+          [:-3], "Cargando modelo de YOLO.")
     model = torch.hub.load('ultralytics/yolov5', 'custom',
                            path=os.path.join(current_dir, 'aztech.pt'))
+
+    print(datetime.now().strftime("%H:%M:%S.%f")[
+          :-3], "Modelo de YOLO cargado correctamente.")
 
     frame = imagen
 
     # Detectar objetos
     detect = model(frame)
     info = []
+    print(datetime.now().strftime("%H:%M:%S.%f")[:-3], "Detectando objetos.")
     info = detect.pandas().xyxy[0]
+    print(datetime.now().strftime("%H:%M:%S.%f")[:-3], "Objetos detectados.")
 
     print(info)
 
@@ -95,7 +119,11 @@ def procesar_imagen():
     if is_detectando_texto:
 
         # Obtener el texto de la imagen
+        print(datetime.now().strftime("%H:%M:%S.%f")[
+              :-3], "Detectando texto con PyTesseract.")
         texto = pytesseract.image_to_string(frame)
+        print(datetime.now().strftime("%H:%M:%S.%f")[
+              :-3], "Texto detectado con PyTesseract.")
 
         texto = limpiar_texto(texto)
 
